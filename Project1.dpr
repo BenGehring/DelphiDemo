78041a1a030aaaeafdeb96de0c77e83a53edd64b
Benjamin Gehring <Ben@BenGehring.com>
2025-04-17 12:24:07 -0400

    Initial commit

---
+++ Project1.dpr
@@ -0,0 +1,14 @@
+program Project1;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit1 in 'Unit1.pas' {mainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TmainForm, mainForm);
+  Application.Run;
+end.
---
+++ Unit1.pas
@@ -0,0 +1,24 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TmainForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  mainForm: TmainForm;
+
+implementation
+
+{$R *.fmx}
+
+end.
